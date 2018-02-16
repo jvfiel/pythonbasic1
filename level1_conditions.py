d0b1a429acbcb7470442171c2edef492bfd971af
jvfiel <jvfiel@bai.ph>
2018-02-17 00:55:21 +0800

    push

---
+++ level1_conditions.py
@@ -0,0 +1,20 @@
+# name = input("Give me your name: ")
+# print("Your name is " + name)
+
+age = 60
+if age == 60:
+    print('old')
+elif age == 40:
+    print('life starts at 40')
+else:
+    print('exception')
+
+switch = 0
+if switch:
+    print('on!')
+else:
+    print('off!')
+
+#Exercise!
+#Make a script that will ask the Name, Gender and Age
+#if Age is 60, print "You are old"
--- level1_input.py
+++ level1_input.py
@@ -1,2 +1,5 @@
 name = input("Give me your name: ")
 print("Your name is " + name)
+
+#Exercise!
+#Make a script that will ask the Name, Gender and Age
